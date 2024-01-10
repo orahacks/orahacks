096e19de86783dca8ca71d9e6108d13a8858b6c0
oravew <liv@oravew.com>
2024-01-10 17:41:05 -0600

    redep

--- app/mainsection.js
+++ app/mainsection.js
@@ -12,7 +12,7 @@ function MainSection() {
       <p>CHICAGO HACKATHON</p>
       {/* Replace 'button-link' with actual paths or functions */}
       <button className="join-btn" onClick={() => openInNewTab("https://form.typeform.com/to/ZnWxoOlH")}>JOIN US</button>
-      <button className="explore-btn" onClick={() => openInNewTab("https://oravew.xyz/winter-is-coming-hackathon-2023-2/")}>LAST SEASON</button>
+      <button className="explore-btn" onClick={() => openInNewTab("https://oravew.xyz/winter-is-coming-hackathon-2023-2/")}>OUR LAST SEASON</button>
       {/* Dates or any other information */}
       <p className="dates">03.29.2023 - 03.31.2023</p>
     </div>
