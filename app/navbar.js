7c87155a31f92e9bea13d4397afd72d29285e588
oravew <155914990+orahacks@users.noreply.github.com>
2024-03-06 11:28:59 -0600

    logo

--- app/navbar.js
+++ app/navbar.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './Navbar.css'; // CSS file for Navbar styling
+import logo from "../public/logo.svg"
+import Image from 'next/image';
 
 function Navbar() {
   return (
     <nav className="Navbar">
       {/* Add your logo or title here */}
-      <div className="logo">ORAHACKS</div>
+      <div className="logo">
+      <Image
+                                    src={logo}
+                                    alt=""
+                                />
+      </div>
       <ul className="nav-links">
         {/* Add your nav items here */}
         <li><a href="https://linktr.ee/orahacks" target="_blank">Join Us</a></li>
